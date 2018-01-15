15d63503e53ab4031d9f6f3c86f5cd08c508d2f4
biezhi <biezhi.me@gmail.com>
2018-01-15 21:19:10 +0800

    :ok_hand: update okhttp3 cookie

---
+++ http-example/okhttp3-example/src/main/java/io/github/biezhi/okhttp3/OkHttp3Example10.java
@@ -0,0 +1,41 @@
+package io.github.biezhi.okhttp3;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.riversun.okhttp3.OkHttp3CookieHelper;
+
+import java.io.IOException;
+
+/**
+ * 设置 Cookie
+ *
+ * @author biezhi
+ * @date 2018/1/15
+ */
+public class OkHttp3Example10 {
+
+    public static void main(String[] args) throws IOException {
+
+        String url = "http://httpbin.org/cookies";
+        OkHttp3CookieHelper cookieHelper = new OkHttp3CookieHelper();
+
+        //force set cookie from the client
+        cookieHelper.setCookie(url, "BID_SESSION", "1233771");
+
+        // init OkHttpClient
+        OkHttpClient client = new OkHttpClient.Builder()
+                .cookieJar(cookieHelper.cookieJar())
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            System.out.println(response.body().string());
+        }
+
+    }
+
+}
