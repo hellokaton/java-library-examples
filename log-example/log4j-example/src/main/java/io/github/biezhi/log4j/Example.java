e24880c8aba18aab4c002d0f95004a1c9f597f31
biezhi <biezhi.me@gmail.com>
2018-01-16 13:20:41 +0800

    :sparkles: add log4j example

---
+++ log-example/log4j-example/src/main/java/io/github/biezhi/log4j/Example.java
@@ -0,0 +1,15 @@
+package io.github.biezhi.log4j;
+
+import org.apache.log4j.Logger;
+
+public class Example {
+    final static Logger LOGGER = Logger.getLogger(Example.class);
+
+    public static void main(String[] args) {
+        LOGGER.debug("我爱编程，编程使我快乐。");
+        LOGGER.info("我爱编程，编程使我快乐。");
+        LOGGER.warn("我爱编程，编程使我快乐。");
+        LOGGER.error("我爱编程，编程使我快乐。");
+    }
+
+}
